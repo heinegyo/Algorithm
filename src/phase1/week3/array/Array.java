6fc45648dcf8a1473365afa27c95c0853a0cb868
heinegyo <heinegyo@gmail.com>
2021-07-07 07:51:55 +0800

    陣列支援泛型

--- src/phase1/week3/array/Array.java
+++ src/phase1/week3/array/Array.java
@@ -4,13 +4,13 @@ package phase1.week3.array;
  * 索引沒有語意，如何表示沒有元素？
  * 如何加入、刪除元素？
  */
-public class Array {
+public class Array<E> {
 
-    private int[] data;
+    private E[] data;
 
     //array中實際的元素數量
     private int size;
-    
+
     /**
      * 無參建構子，預設陣列的容量capacity為10
      */
@@ -23,8 +23,9 @@ public class Array {
      *
      * @param capacity
      */
-    public Array(int capacity) {
-        data = new int[capacity];
+    public Array(int capacity){
+        //data = new E[capacity]; //java 中無法
+        data = (E[])(new Object[capacity]);
         size = 0;
     }
 
@@ -44,17 +45,17 @@ public class Array {
     }
 
     //在所有元素前面加入一個元素
-    public void addFirst(int e) {
+    public void addFirst(E e) {
         add(0, e);
     }
 
     //向所有元素後加入一個新元素
-    public void addLast(int e) {
+    public void addLast(E e) {
         add(size, e);
     }
 
     //在第index個位置插入一個新元素e
-    public void add(int index, int e) {
+    public void add(int index, E e) {
 
         if (size == data.length)
             throw new IllegalArgumentException("AddLast failed.Array is full.");
@@ -72,23 +73,23 @@ public class Array {
     }
 
     //獲取index索引位置的元素
-    int get(int index) {
+    public E get(int index) {
         if (index < 0 || index >= size)
             throw new IllegalArgumentException("Get failed.Index is illegal");
         return data[index];
     }
 
     //修改index髓飲位置的元素為e
-    void set(int index, int e) {
+    public void set(int index, E e) {
         if (index < 0 || index >= size)
             throw new IllegalArgumentException("Set failed.Index is illegal");
         data[index] = e;
     }
 
     //查詢陣列中是否有元素e
-    public boolean contains(int e) {
+    public boolean contains(E e) {
         for (int i = 0; i < size; i++) {
-            if (data[i] == e) {
+            if (data[i].equals(e)) {
                 return true;
             }
         }
@@ -96,9 +97,9 @@ public class Array {
     }
 
     //查詢陣列中元素e所在的索引，如果不存在元素e，則回傳-1
-    public int find(int e) {
+    public int find(E e) {
         for (int i = 0; i < size; i++) {
-            if (data[i] == e) {
+            if (data[i].equals(e)) {
                 return i;
             }
         }
@@ -106,29 +107,30 @@ public class Array {
     }
 
     //從陣列中刪除index位置的元素，回傳刪除的元素
-    public int remove(int index) {
+    public E remove(int index) {
         if (index < 0 || index >= size)
             throw new IllegalArgumentException("Remove failed.Index is illegal");
-        int ret = data[index];
+        E ret = data[index];
         for (int i = index + 1; i < size; i++) {
             data[i - 1] = data[i];
         }
         size--;
+        data[size] = null; //loitering objects != memory leak
         return ret;
     }
 
     //從陣列中刪除第一個元素，回傳刪除的元素
-    public int removeFirst() {
+    public E removeFirst() {
         return remove(0);
     }
 
     //從陣列中刪除最後一個元素，回傳刪除的元素
-    public int removeLast() {
+    public E removeLast() {
         return remove(size - 1);
     }
 
     //從陣列中刪除元素e
-    public void removeElement(int e) {
+    public void removeElement(E e) {
         int index = find(e);
         if (index != -1)
             remove(index);
--- src/phase1/week3/array/Main.java
+++ src/phase1/week3/array/Main.java
@@ -1,26 +1,16 @@
 package phase1.week3.array;
 
+import phase1.week2.selectionSort.Student;
+
 public class Main {
     public static void main(String[] args) {
-        Array arr = new Array(20);
-        for (int i = 0; i < 10; i++) {
-            arr.addLast(i);
-        }
+
+        Array<phase1.week2.selectionSort.Student> arr = new Array<>();
+        arr.addLast(new Student("Alice",100));
+        arr.addLast(new Student("Bob",66));
+        arr.addLast(new Student("Charlie",88));
+
         System.out.println(arr);
 
-        arr.add(1, 100);
-        System.out.println(arr);
-
-        arr.addFirst(-1);
-        System.out.println(arr);
-
-        arr.remove(2);
-        System.out.println(arr);
-
-        arr.removeElement(4);
-        System.out.println(arr);
-
-        arr.removeFirst();
-        System.out.println(arr);
     }
 }
