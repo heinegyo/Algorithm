0c93f7b2d02c43ed70855aaa873f9c0325a245b1
heinegyo <heinegyo@gmail.com>
2021-07-28 07:49:09 +0800

    LinkedList CRU

--- src/phase1/week4/linkedlist/LinkedList.java
+++ src/phase1/week4/linkedlist/LinkedList.java
@@ -60,11 +60,83 @@ public class LinkedList<E> {
 
     //在鏈表頭增加新的元素e
     public void addFirst(E e) {
-        add(0,e);
+        add(0, e);
     }
 
     //在鏈表末尾增加新的元素
     public void addLast(E e) {
         add(size, e);
+    }
+
+    //獲得鏈表的第index(0-based)個位置的元素
+    //在鏈表中不是一個常見情況，練習用
+    public E get(int index) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("add failed.Illegal index");
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        return cur.e;
+    }
+
+    //獲得鏈表的第一個元素
+    public E getFirst() {
+        return get(0);
+    }
+
+    //獲得鏈表的最後一個元素
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    //修改鏈表的第index(0-based)個位置的元素
+    //在鏈表中不是一個常見的情況，練習用
+    public void set(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Set failed.Illegal index");
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+    //查找鏈表中是否有元素e
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e))
+                return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+
+        // Node cur = dummyHead.next;
+        // while (cur != null) {
+        //     res.append(cur + "->");
+        //     cur = cur.next;
+        // }
+
+        for (Node cur = dummyHead.next; cur != null; cur = cur.next)
+            res.append(cur + "->");
+        res.append("Null");
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(2,666);
+        System.out.println(linkedList );
     }
 }
