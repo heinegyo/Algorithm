365dbdd4da9a820a4c1e2dbdc5574b945288803f
heinegyo <heinegyo@gmail.com>
2021-08-21 15:14:27 +0800

    No203 使用dummyHead

--- src/phase1/week4/linkedlist/leetcode/Solution2.java
+++ src/phase1/week4/linkedlist/leetcode/Solution2.java
@@ -3,21 +3,18 @@ package phase1.week4.linkedlist.leetcode;
 /**
  * Remove Linked List Elements
  * LeetCode 203
- * 解說版
+ * 使用dummyHead
  */
-public class Solution {
+public class Solution2 {
     public ListNode removeElements(ListNode head, int val) {
-        //先處理head節點，
-            //使用循環是避免第二個節點也等於val
-        while (head != null && head.val == val) {
-            head = head.next;
-        }
 
-        if (head == null)
-            return null;
+        //使用dummyHead就不用把head和中間的節點分開處理
+        //dummyHead的val不重要，目的只是在head前加個節點
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
 
         //處理完head之後，用prev處理中後段的節點
-        ListNode prev = head;
+        ListNode prev = dummyHead;
         while (prev.next != null) {
             if (prev.next.val == val) {
                 prev.next = prev.next.next;
@@ -25,6 +22,7 @@ public class Solution {
                 prev = prev.next;
             }
         }
-        return head;
+        //不能直接把dummyHead return回去，dummyHead是對外隱藏的
+        return dummyHead.next;
     }
 }
