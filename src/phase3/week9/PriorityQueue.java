4878e1f2805becc49fa43ff4de7c28a484b93e57
heinegyo <heinegyo@gmail.com>
2022-05-31 21:33:34 +0800

    PriorityQueue

---
+++ src/phase3/week9/PriorityQueue.java
@@ -0,0 +1,39 @@
+package phase3.week9;
+
+import phase1.week3.queue.Queue;
+
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+    private MaxHeap<E> maxHeap;
+
+    public PriorityQueue(){
+        maxHeap = new MaxHeap<>();
+    }
+
+    @Override
+    public int getSize() {
+        return maxHeap.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return maxHeap.isEmpty();
+    }
+
+    @Override
+    public E getFront() {
+        return maxHeap.findMax();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        maxHeap.add(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return maxHeap.extractMax();
+    }
+
+
+}
