ca76855ae3324e47ba630b3b00d1206cd807a858
heinegyo <heinegyo@gmail.com>
2022-06-03 14:53:17 +0800

    使用Java2k7

--- src/phase3/week9/LeastNumbers.java
+++ src/phase3/week9/LeastNumbers.java
@@ -1,6 +1,8 @@
 package phase3.week9;
 
 
+import java.util.Collections;
+
 public class LeastNumbers {
     //找出arr中最小的k個數
     public int[] getLeastNumbers(int[] arr,int k){
@@ -18,6 +20,26 @@ public class LeastNumbers {
         int[] res = new int[k];
         for (int i = 0; i < k; i++)
             res[i] = pq.dequeue();
+
+        return res;
+    }
+
+    //使用Java中的PriorityQueue
+    public int[] getLeastNumbers2(int[] arr,int k){
+
+        java.util.PriorityQueue<Integer> pq = new java.util.PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < k; i++)
+            pq.add(arr[i]);
+
+        for (int i = k; i < arr.length; i++)
+            if(!pq.isEmpty() && arr[i] < pq.peek()){
+                pq.remove();
+                pq.add(arr[i]);
+            }
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++)
+            res[i] = pq.remove();
 
         return res;
     }
--- src/phase3/week9/LeetCode215.java
+++ src/phase3/week9/LeetCode215.java
@@ -1,5 +1,7 @@
 package phase3.week9;
 
+import java.util.PriorityQueue;
+
 public class LeetCode215 {
     public int findKthLargest(int[] nums, int k) {
 
@@ -13,4 +15,21 @@ public class LeetCode215 {
 
         return pq.findMin();
     }
+
+    //使用Java中的PriorityQueue
+    public int findKthLargest2(int[] nums, int k) {
+
+        PriorityQueue<Integer> pq  = new PriorityQueue<>();
+        for (int i = 0; i < k; i++)
+            pq.add(nums[i]);
+
+        for (int i = k; i < nums.length; i++)
+            if (!pq.isEmpty() && nums[i] > pq.peek()) {
+                pq.remove(nums[i]);
+                pq.add(nums[i]);
+            }
+
+        return pq.peek();
+    }
+
 }
