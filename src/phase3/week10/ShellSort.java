a759e9a88eb976e5983b89ff69c1ad6e171b561d
heinegyo <heinegyo@gmail.com>
2022-06-16 21:22:39 +0800

    優化ShellSort

--- src/phase3/week10/ShellSort.java
+++ src/phase3/week10/ShellSort.java
@@ -16,29 +16,44 @@ public class ShellSort {
         int h = data.length / 2;
         while (h >= 1) {
 
-            for (int start = 0; start < h; start++) {//遍歷每個子陣列的起始位置
-
-                //對data[start,start + h , start + 2h ,start + 3h ...],進行InsertionSort
-                for (int i = start + h; i < data.length; i += h) {
-                    E t = data[i];
-                    int j;
-                    for (j = i; j - h >= 0 && t.compareTo(data[j - h]) < 0; j -= h)
-                        data[j] = data[j - h];
-                    data[j] = t;
-                }
+            //對data[h,n),進行InsertionSort
+            for (int i = h; i < data.length; i++) {
+                E t = data[i];
+                int j;
+                for (j = i; j - h >= 0 && t.compareTo(data[j - h]) < 0; j -= h)
+                    data[j] = data[j - h];
+                data[j] = t;
             }
             h /= 2;
         }
     }
 
+    public static <E extends Comparable<E>> void sort2(E[] data) {
+
+        int h = 1;
+        while (h < data.length) h = h * 3 + 1;
+        //1,4,13,40...
+        while (h >= 1) {
+
+            //對data[h,n),進行InsertionSort
+            for (int i = h; i < data.length; i++) {
+                E t = data[i];
+                int j;
+                for (j = i; j - h >= 0 && t.compareTo(data[j - h]) < 0; j -= h)
+                    data[j] = data[j - h];
+                data[j] = t;
+            }
+            h /= 3;
+        }
+    }
+
     public static void main(String[] args) {
-        int n = 100000;
-        Integer[] arr = ArrayGenerator.generateRandomArray(n,n);
-        Integer[] arr2 = Arrays.copyOf(arr,arr.length);
+        int n = 5000000;
+        Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
+        Integer[] arr2 = Arrays.copyOf(arr, arr.length);
 
-        SortingHelper.sortTest("ShellSort",arr);
+        SortingHelper.sortTest("ShellSort", arr);
         // SortingHelper.sortTest("InsertionSort",arr2);
-        SortingHelper.sortTest("MergeSort",arr);
-
+        SortingHelper.sortTest("ShellSort2", arr2);
     }
 }
